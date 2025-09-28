1640dc1646e9c19354fd8bd808b04d35fb726983
Adarsh Agrawal <adarshagrawal105@gmail.com>
2025-09-27 18:19:43 -0700

    4 product_parser method completions

--- product_parser.cpp
+++ product_parser.cpp
@@ -130,8 +130,8 @@ std::string ProductBookParser::categoryID()
  */
 Product* ProductBookParser::makeProduct()
 {
-
-
+    Product* retVal = new Book("book", prodName_, price_, qty_, isbn_, author_);
+    return retVal;
 }
 
 
@@ -185,9 +185,8 @@ std::string ProductClothingParser::categoryID()
  */
 Product* ProductClothingParser::makeProduct()
 {
-
-
-
+    Product* retVal = new Clothing("clothing", prodName_, price_, qty_, size_, brand_);
+    return retVal; 
 }
 
 
@@ -245,6 +244,6 @@ std::string ProductMovieParser::categoryID()
  */
 Product* ProductMovieParser::makeProduct()
 {
-
-
+    Product* retVal = new Movie("movie", prodName_, price_, qty_, genre_, rating_);
+    return retVal; 
 }
